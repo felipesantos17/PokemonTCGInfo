c2f04706b5de949aa4eace92cb3a6c3e841fb83b
Felipe Costa Santos <fezto.17@gmail.com>
2025-01-12 23:45:58 -0300

    First commit

---
+++ PokemonTCGInfos/PokemonTCGInfosApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonTCGInfosApp.swift
+//  PokemonTCGInfos
+//
+//  Created by Felipe Santos on 12/01/25.
+//
+
+import SwiftUI
+
+@main
+struct PokemonTCGInfosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
